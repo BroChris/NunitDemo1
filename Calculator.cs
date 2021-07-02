b1001ad0d2c0c7141cfeed3ccf67c9ee1ef22e97
Chris Chinazo Okafor <chris.okaforce@gmail.com>
2021-07-02 17:32:26 +0100

    Add project files.

---
+++ Calculator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.Intrinsics.X86;
+
+namespace NunitDemo
+{
+    public class Calculator
+    {
+        public int Addition(int firstNumuber, int SecondNumber)
+        {
+            return firstNumuber + SecondNumber;
+           
+        }
+
+        public int Subtraction(int firstNumuber, int SecondNumber)
+        {
+            if (firstNumuber < SecondNumber)
+            {
+                throw new ArgumentException($"First Number {firstNumuber} is Lesser than the Second Number{SecondNumber}");
+            }
+            return firstNumuber - SecondNumber;
+        }
+
+    }
+}
